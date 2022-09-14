deea9f4f121092821031727b2f4f98ee262a0d2f
Daniel Andrews <daniel.korienko@gmail.com>
2022-09-14 17:06:19 +0300

    Update

---
+++ client/src/components/featured/Featured.tsx
@@ -0,0 +1,16 @@
+import './Featured.css'
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+      <div className='featuredItem'>
+        <img src='' alt='' className='featuredImg' />
+        <div className="featuredTitles">
+            <h1>Dublin</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Featured
